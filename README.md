722dc021efe2293dc71352ebd4e640ab0a5edd64
Zach Smith <zach.smith@silviaterra.com>
2021-12-07 03:26:03 -0500

    more snake

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Advent of Code 2021
 
-In key of C.
+In key of C. Now with more snake.
 
 ```shell
 > make
 > make run
+```
+ or
+
+```python
+> python advent.py
 ```
---
+++ advent.py
@@ -0,0 +1,15 @@
+import treeofknowledge as tok
+
+welcome = '''######################################
+#
+#
+# ADVENT OF CODE 2021
+#
+#
+######################################
+'''
+print(welcome)
+
+print('---Day 7---')
+print(f'Fuel :{tok.day7.solve1()}')
+print(f'Fuel :{tok.day7.solve2()}')
