fcd10d42c787a09fff02fd151923d8f9c94732f3
Zach Smith <zach.smith@silviaterra.com>
2021-12-15 01:15:54 -0500

    organize

--- treeofknowledge/day15.py
+++ treeofknowledge/day15.py
@@ -19,26 +19,30 @@ def check_valid(i, j, mat):
     else:
         return False
 
-def solve(path):
-    mat = read_data(path)
+def search(queue, mat):
+    # another breadth first search with a couple rules
     score = 1e15 * np.ones_like(mat)
     score[0,0] = 0
-    start = [0,0,0,0] # i, j, cost, prev i, prev j
-    end = [i - 1 for i in mat.shape]
-    queue = [start]
     while len(queue) > 0:
         ij = queue.pop(0)
         for neigh in neighbors:
             i = ij[0] + neigh[0]
             j = ij[1] + neigh[1]
-            if check_valid(i, j, mat) and (i != ij[2] or j != ij[3]): # don't got backwards
+            if check_valid(i, j, mat) and (i != ij[2] or j != ij[3]): # don't go backwards
                 new_score = score[ij[0], ij[1]] + mat[i, j]
-                if new_score < score[i, j]:
+                if new_score < score[i, j]: # build path if cheaper
                     score[i, j] = new_score
                     queue += [[i, j, ij[0], ij[1]]]
-    print(score[end[0], end[1]])
+    return score
 
-    # big time
+def solve(path):
+    mat = read_data(path)
+    start = [0,0,0,0] # i, j, prev i, prev j
+    queue = [start]
+    score = search(queue, mat)
+    print(score[-1, -1])
+
+    # big time, expand the map
     factor = 5
     n, m = mat.shape
     N, M = n * factor, m * factor
@@ -48,21 +52,9 @@ def solve(path):
             to_fill = mat + i + j
             to_fill[to_fill > 9] = to_fill[to_fill > 9] % 9 # wrap around top values
             bmat[(i * n):(i * n + n), (j * n):(j * n + n)] = to_fill
-    
-    score = 1e15 * np.ones_like(bmat)
-    score[0,0] = 0
+
     start = [0,0,0,0] # i, j, prev i, prev j
-    end = [i - 1 for i in bmat.shape]
     queue = [start]
-    while len(queue) > 0:
-        ij = queue.pop(0)
-        for neigh in neighbors:
-            i = ij[0] + neigh[0]
-            j = ij[1] + neigh[1]
-            if check_valid(i, j, bmat) and (i != ij[2] or j != ij[3]):
-                new_score = score[ij[0], ij[1]] + bmat[i, j]
-                if new_score < score[i, j]:
-                    score[i, j] = new_score
-                    queue += [[i, j, ij[0], ij[1]]]
-    print(score[end[0], end[1]])
+    score = search(queue, bmat)
+    print(score[-1, -1])
     print("\n")
