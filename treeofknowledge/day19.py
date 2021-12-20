4596d082d5ff96ab89a0f30b9fbb9c3ba46c0438
Zach <ezke.smith@gmail.com>
2021-12-19 22:45:02 -0500

    working on particle filter

--- treeofknowledge/day19.py
+++ treeofknowledge/day19.py
@@ -1,4 +1,5 @@
 import numpy as np
+from itertools import product
 
 def read(path):
     arrays = []
@@ -15,8 +16,67 @@ def read(path):
                 new_array += [[int(x) for x in line.strip().split(',')]]
     return arrays
 
+rotations = list(product([0,1,2,3], [0,1,3], [0,2]))
 
+def rotate(x, axis, theta):
+    c = np.cos(theta)
+    s = np.sin(theta)
+    R = np.array([
+        [
+            [1,0,0],
+            [0,c,-s],
+            [0,s,c]
+        ],
+        [
+            [c,0,-s],
+            [0,1,0],
+            [s,0,c]
+        ],
+        [
+            [c,-s,0],
+            [s,c,0],
+            [0,0,1]
+        ]
+    ])
+    return R[axis,:,:] @ x
 
+class ParticleFilter:
+    def __init__(self, truth, comp, n_particles, iters):
+        self.truth = truth
+        self.comp = comp
+        self.N = n_particles
+        self.iters = iters
+
+        # generate particles
+        self.particles = np.random.randint(-2000, 2000, size=(self.N, 3))
+        self.weights = np.ones(self.N)
+
+    def fit(self):
+        self.find_best_rotation()
+        for _ in self.iters:
+            self.jitter()
+            self.score()
+            self.select()
+
+    def jitter(self):
+        self.weights = np.ones(self.N)
+        self.particles += self.random.randint(-1, 1, size=(self.N, 3))
+
+    def score(self):
+        min_scores = np.ones(self.N) * float('inf')
+        # find pairwise best match of points
+        
+
+    def find_best_rotation(self):
+        for rotation in rotations:
+            y = self.comp.copy()
+            for d, amount in enumerate(rotation):
+                y = rotate(y, d, amount * np.pi * 0.5)
+
+    def select(self):
+        keep_ix = np.random.choice(self.N, self.N, replace=True, p=self.weights)
+        self.particles = self.particles[keep_ix,:]
+        self.weights = self.weights[keep_ix]
 
 def solve(path):
     arrays = read(path)
